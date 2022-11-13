b8d7cb61af5afec5eb5481e94a16f2598881b3b4
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-13 19:00:31 +0530

    Close #4

--- src/main/java/controller/WelcomeFormController.java
+++ src/main/java/controller/WelcomeFormController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class WelcomeFormController {
 
+    public AnchorPane pneWelcome;
+    public Button btnRegister;
+    public Button btnLogin;
+
+    public void btnRegister_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogin_OnAction(ActionEvent actionEvent) {
+    }
 }
