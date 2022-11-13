5393845cadc31939f9e61e79af56dc31cb46c927
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-13 18:04:55 +0530

    Close #1

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,5 +1,12 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class
 AppInitializer extends Application {
@@ -9,7 +16,11 @@ AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/SplashScreenForm.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
