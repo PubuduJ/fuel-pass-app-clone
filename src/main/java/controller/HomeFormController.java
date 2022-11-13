79530c234debc88fa11c6d1649d8353addf1f5b4
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-13 18:46:45 +0530

    Close #3

--- src/main/java/controller/HomeFormController.java
+++ src/main/java/controller/HomeFormController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class HomeFormController {
+    public AnchorPane pneContainer;
+    public AnchorPane pneLogin;
+    public AnchorPane pneHomeForm;
+
+    public void pneHome_OnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void imgLogo_OnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void pneLogin_OnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void pneLogin_OnMouseClicked(MouseEvent mouseEvent) {
+    }
 }
