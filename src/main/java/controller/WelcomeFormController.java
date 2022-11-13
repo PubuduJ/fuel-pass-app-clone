80e1bc96316cc4ed8ebb5fba1e07ddcaf17ae601
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-13 19:03:57 +0530

    Implement welcome form initialize logic

--- src/main/java/controller/WelcomeFormController.java
+++ src/main/java/controller/WelcomeFormController.java
@@ -1,14 +1,23 @@
 package controller;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class WelcomeFormController {
 
     public AnchorPane pneWelcome;
     public Button btnRegister;
     public Button btnLogin;
+
+    public void initialize() {
+        FadeTransition fd = new FadeTransition(Duration.millis(1000),pneWelcome);
+        fd.setFromValue(0);
+        fd.setToValue(1);
+        fd.playFromStart();
+    }
 
     public void btnRegister_OnAction(ActionEvent actionEvent) {
     }
