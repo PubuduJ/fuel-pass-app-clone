961fe9e3c0cbd42d690e2756ce1f02b0b5aa5212
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-14 10:46:03 +0530

    Complete admin login form section

--- src/main/java/controller/AdminLoginFormController.java
+++ src/main/java/controller/AdminLoginFormController.java
@@ -3,13 +3,28 @@ package controller;
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
+import util.Navigation;
+import util.Routes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class AdminLoginFormController {
     public AnchorPane pneAdminLoginForm;
     public PasswordField txtPassword;
+
+    private static final String ADMIN_PASSWORD = "admin";
+
+    private static int attempts = 3;
 
     public void initialize() {
         FadeTransition fd = new FadeTransition(Duration.millis(1000),pneAdminLoginForm);
@@ -19,6 +34,36 @@ public class AdminLoginFormController {
         Platform.runLater(txtPassword::requestFocus);
     }
 
-    public void txtPassword_OnAction(ActionEvent actionEvent) {
+    public void txtPassword_OnAction(ActionEvent actionEvent) throws IOException {
+        if (!txtPassword.getText().equals(ADMIN_PASSWORD)) {
+            if (attempts == 0) {
+                new Alert(Alert.AlertType.ERROR,"You have reached maximum number of attempts...").showAndWait();
+                Platform.exit();
+                return;
+            }
+            URL resource = this.getClass().getResource("/audio/alertSound.mp3");
+            Media media = new Media(resource.toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Admin Password. You have " + attempts + " more attempts to try again");
+            attempts -= 1;
+
+            InputStream resourceAsStream = this.getClass().getResourceAsStream("/img/padlock.png");
+            Image image = new Image(resourceAsStream);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(45);
+            imageView.setFitHeight(45);
+            alert.setGraphic(imageView);
+            alert.setTitle("Access Denied");
+            alert.setHeaderText("Invalid Login Credentials");
+            alert.showAndWait();
+
+            mediaPlayer.dispose();
+
+            txtPassword.requestFocus();
+            return;
+        }
+        Navigation.navigate(Routes.CONTROL_CENTER);
     }
 }
